88645bcf1fa5eb723a63ed49106c059ac2d8af1f
damianbednarz1984 <damianbednarz1984@gmail.com>
2021-03-18 19:29:13 +0000

    Update app.py
    delete

--- app.py
+++ app.py
@@ -39,8 +39,7 @@ def update():
 def delete():
   id2 = request.args.get('id')
   cur = mysql.connection.cursor() 
-<<HEAD
-  s="delete from students where studentID ='%s" %id2
+  s="delete from students where studentID ='%s'" %id2
   cur.execute(s)
   mysql.connection.commit()
 
