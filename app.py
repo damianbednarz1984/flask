f6f34d42e433edabdf56d577fdae38c3817073b6
damianbednarz1984 <damianbednarz1984@gmail.com>
2021-03-18 19:14:12 +0000

    Update app.py
    add placeholder update

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ app.config['MYSQL_DB'] = 'student'
 app.config['MYSQL_HOST'] = 'localhost' #for now
 mysql.init_app(app)
 
+
 @app.route("/add") #Add Student
 def add():
   name = request.args.get('name')
@@ -25,6 +26,17 @@ def add():
   mysql.connection.commit()
 
   return '{"Result":"Success"}'
+  @app.route("/update") #Add Student
+def update():
+  name = request.args.get('name')
+  email = request.args.get('email')
+  cur = mysql.connection.cursor() #create a connection to the SQL instance
+  #s='''INSERT INTO students(studentName, email) VALUES('{}','{}');'''.format(name,email)
+  #cur.execute(s)
+  #mysql.connection.commit()
+
+  return '{"Result":"not done"}'
+
 @app.route("/") #Default - Show Data
 def hello(): # Name of the method
   cur = mysql.connection.cursor() #create a connection to the SQL instance
